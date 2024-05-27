c1b13614722de4218bc13fb03e01a072b4652c3d
AGPatriota <patriota.alexandre@gmail.com>
2024-05-27 13:39:35 -0300

    Generating with flexible initial context sizes

--- R/GPT.R
+++ R/GPT.R
@@ -28,13 +28,9 @@ GPT <- torch::nn_module(
     self$drop0 <- torch::nn_dropout(p = p0)
   },
   forward = function(x) {
-    x1 <- torch::torch_arange(1, self$block_size, dtype = torch::torch_int(), device = x$device )$unsqueeze(1)
-    wei <- torch::torch_zeros(self$block_size, self$block_size,dtype = torch::torch_bool(), device = x$device)
+    x1 <- torch::torch_arange(1, x$size(2), dtype = torch::torch_int(), device = x$device )$unsqueeze(1)
+    wei <- torch::torch_zeros(x$size(2), x$size(2),dtype = torch::torch_bool(), device = x$device)
     wei[upper.tri(wei)] <- 1
-    if (x$size(2) < self$block_size) {
-      zeros <- torch::torch_tensor(rep(50257, self$block_size - x$size(2)), dtype = torch::torch_int(), device = x$device)$unsqueeze(1)$expand(c(x$size(1),-1))
-      x <- torch::torch_cat(list(x,zeros), 2)
-    }
     output <- self$wte(x) + self$wpe(x1)
     output <- self$drop0(output)
     for (j in 1:self$N) {
